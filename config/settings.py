43d2c5d12c816f04208c5dd6e1d522e53a4e84a3
Mohammed Ameein <example@gmail.com>
2026-09-13 01:48:33 +0300

    Allow PythonAnywhere hostname

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ','.join([
     '192.168.100.86',
     'mohammedameein-realestate.com',
     'www.mohammedameein-realestate.com',
+    'mohammedamein.pythonanywhere.com',
      '.trycloudflare.com',
     '.up.railway.app',
     '.railway.app',
